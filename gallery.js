62b905da9123f8738cccdca46ce0f2e02db0e559
Aryan Jha <66917687+aryanjha06@users.noreply.github.com>
2021-01-22 22:07:52 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,19 @@
+$('.filter a').click(function (e) {
+    e.preventDefault();
+    var a = $(this).attr('href');
+    a = a.substr(1);
+    $('.sets a').each(function () {
+        if (!$(this).hasClass(a) && a != 'all')
+            $(this).addClass('hide');
+        else
+            $(this).removeClass('hide');
+    });
+
+});
+
+$('.sets a').click(function (e) {
+    e.preventDefault();
+    var $i = $(this);
+    $('.sets a').not($i).toggleClass('pophide');
+    $i.toggleClass('pop');
+});
